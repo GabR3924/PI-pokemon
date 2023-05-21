c30d593df14ba69268602819e2633e333f8bfbc6
GabR3924 <garc2004@gmail.com>
2023-05-21 14:40:01 -0400

    menos

--- client/src/componentes/Explorador/Crear/Crear.jsx
+++ client/src/componentes/Explorador/Crear/Crear.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
 
-const Crear = () => {
+function Crear() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [vida, setVida] = useState("");
+  const [ataque, setAtaque] = useState("");
+  const [defensa, setDefensa] = useState("");
+  const [velocidad, setVelocidad] = useState("");
+  const [altura, setAltura] = useState("");
+  const [peso, setPeso] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:3001/pokemon/new", {
+        name,
+        image,
+        vida,
+        ataque,
+        defensa,
+        velocidad,
+        altura,
+        peso,
+      })
+      .then((response) => {
+        setMessage("Pokémon creado correctamente");
+      });
+  };
+
   return (
-    <div>Crear</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Nombre"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Imagen"
+        value={image}
+        onChange={(event) => setImage(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Vida"
+        value={vida}
+        onChange={(event) => setVida(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ataque"
+        value={ataque}
+        onChange={(event) => setAtaque(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Defensa"
+        value={defensa}
+        onChange={(event) => setDefensa(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Velocidad"
+        value={velocidad}
+        onChange={(event) => setVelocidad(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Altura"
+        value={altura}
+        onChange={(event) => setAltura(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Peso"
+        value={peso}
+        onChange={(event) => setPeso(event.target.value)}
+      />
+      <button type="submit">Crear</button>
+    </form>
+  );
 }
 
-export default Crear
+export default Crear;
