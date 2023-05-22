03443812c41dba5b659f023da9c7648840f14c3c
GabR3924 <garc2004@gmail.com>
2023-05-22 15:53:07 -0400

    detail

--- client/src/componentes/Explorador/Card/Card.jsx
+++ client/src/componentes/Explorador/Card/Card.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import css from './Card.module.css'
 import {Link} from 'react-router-dom'
 
-const Card = ({id, name, image, height, weight, types}) => {
+const Card = ({ name, image,types}) => {
   return (
     <div className={css.section}>
-        <h2>id:{id}</h2>
-         <h2> <Link to='/card/detail'>{name}</Link></h2>
-         <img src={image} alt="" />
-         <h2>height:{height}</h2>
-         <h2>weight:{weight}</h2>
-         <h2>types:{types}</h2>
+        <img src={image} alt="" />
+         <h1> <Link to='/card/detail'>{name}</Link></h1>
+         <h2>Tipo: {types ? types.join(', ') : ''}</h2>
     </div>
   )
 }
