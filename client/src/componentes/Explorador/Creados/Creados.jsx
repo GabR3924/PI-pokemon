559189d7a7801fd1fea1fe7bf97ef35708806f76
GabR3924 <garc2004@gmail.com>
2023-05-22 22:34:42 -0400

    validations

--- client/src/componentes/Explorador/Creados/Creados.jsx
+++ client/src/componentes/Explorador/Creados/Creados.jsx
@@ -4,11 +4,17 @@ import Card from "../Card/Card.jsx";
 import css from "./Creados.module.css";
 import Nav from "../../Nav/Nav.jsx";
 import { Link } from "react-router-dom";
+import Modal from "../Modal/Modal";
+import CardDetail from "../CardDetail/CardDetail.jsx";
 
 const Creados = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
-  // Este llamado se ejecutará solo una vez, cuando el componente se monte
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
   useEffect(() => {
     axios.get("http://localhost:3001/pokemondb").then((response) => {
       setPokemons(response.data.data);
@@ -18,13 +24,35 @@ const Creados = () => {
   return (
     <div className={css.section}>
       <Nav />
-      <div><Link to='/creados/new'>Crear</Link></div>
-      <div>
-        {console.log(pokemons)}
-        {Array.isArray(pokemons) &&
-          pokemons.map(({ id, name, image }) => (
-            <Card key={id} id={id} name={name} image={image} />
-          ))}
+      <div className={css.pokedb}>
+        <div className={css.create}>
+          <Link to="/creados/new">Crear</Link>
+        </div>
+        <div className={css.db}>
+          {/* {console.log(pokemons)} */}
+          {Array.isArray(pokemons) &&
+            pokemons.map(({ id, name, image }) => (
+              <Card
+                key={id}
+                id={id}
+                name={name}
+                image={image}
+                onOpenModal={() => {
+                  handleOpenModal();
+                  setSelectedPokemon({ name, image });
+                }}
+                onCloseModal={handleCloseModal}
+              />
+            ))}
+        </div>
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          {selectedPokemon && (
+            <CardDetail
+              name={selectedPokemon.name}
+              image={selectedPokemon.image}
+            />
+          )}
+        </Modal>
       </div>
     </div>
   );
